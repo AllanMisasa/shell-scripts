f3bd0d12521d71f4f1e183d0f7a761ece1bce74a
AllanMisasa <lesvoyagenostalgie@gmail.com>
2025-05-15 17:28:52 +0200

    Cleanup of documentconverter code and proper echo

--- documentconverter/pptx-killer.sh
+++ documentconverter/pptx-killer.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Required:
+#   pptx2md - pip install pptx2md https://github.com/ssine/pptx2md
+#   marp-cli - https://github.com/marp-team/marp-cli/releases
+#   pandoc - 
+
 for file in *; do
   ext="${file##*.}"
-    echo $ext
     if [[ $ext == pptx ]]; then
       pptx2md "${file}" -o "${file%.*}".md -i images --enable-slides
       marp "${file%.*}.md" -o "${file%.*}.html" --allow-local-files
     elif [[ $ext == docx ]]; then
-      pandoc -f docx -t markdown "${file%.*}.docx" -o "${file%.*}.md"
-      #marp "${file%.*}.md" -o "${file%.*}.html" --allow-local-files
+      pandoc -f docx -t gfm --wrap=none --extract-media=images -s "${file%.*}.docx" -o "${file%.*}.md"
     fi
+  echo "Conversion done."
 done
