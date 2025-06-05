9f3231215fdaae88846f4a7d56237e917004473c
Allan Misasa Nielsen <lesvoyagenostalgie@gmail.com>
2025-06-05 16:00:58 +0200

    Update nvencFolder.sh

--- ffmpeg/nvencFolder.sh
+++ ffmpeg/nvencFolder.sh
@@ -8,8 +8,8 @@ for file in *.mkv *.mp4; do
         echo "Processing $file..."
         
         # Encode the file to HEVC format using ffmpeg
-        # sudo ffmpeg -i "$file" -c:v hevc_nvenc -vtag hvc1 "${filename}x265.mp4"
-        sudo ffmpeg -i "$file" -c:v hevc_nvenc -c:s copy -map 0 -vtag hvc1 "${filename}x265.mkv"
+        # sudo ffmpeg -i "$file" -c:v hevc_nvenc -vtag hvc1 "${filename}x265.mp4" # Preserves just the 1st channel of audio, and no subtitles
+        sudo ffmpeg -i "$file" -c:v hevc_nvenc -c:s copy -map 0 -vtag hvc1 "${filename}x265.mkv" # Preserves original audio & subtitle tracks
         
         echo "Finished encoding $file to ${filename}.mp4"
     fi
