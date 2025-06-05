d21c1bde95219633518e99d19c614ea43436f038
Allan Misasa Nielsen <lesvoyagenostalgie@gmail.com>
2025-06-05 15:58:57 +0200

    Update nvencFolder.sh

--- ffmpeg/nvencFolder.sh
+++ ffmpeg/nvencFolder.sh
@@ -8,7 +8,8 @@ for file in *.mkv *.mp4; do
         echo "Processing $file..."
         
         # Encode the file to HEVC format using ffmpeg
-        sudo ffmpeg -i "$file" -c:v hevc_nvenc -vtag hvc1 "${filename}x265.mp4"
+        # sudo ffmpeg -i "$file" -c:v hevc_nvenc -vtag hvc1 "${filename}x265.mp4"
+        sudo ffmpeg -i "$file" -c:v hevc_nvenc -c:s copy -map 0 -vtag hvc1 "${filename}x265.mkv"
         
         echo "Finished encoding $file to ${filename}.mp4"
     fi
